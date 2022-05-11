2c6775b6be7bdef1d8ee3bf6709c762362148454
gopaGit <104909447+gopaGit@users.noreply.github.com>
2022-05-11 06:22:31 +0530

    Time: 31 ms (54.97%), Space: 19.3 MB (58.82%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j=0,k=0;
+        
+        for(int i=0;i<nums.size();i++){
+           if(nums[i]!=0) {
+               nums[j++]=nums[i];
+           }
+        }
+        for(int k=j;k<nums.size();k++) nums[k]=0;
+                           
+    }
+};
