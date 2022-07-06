fbad67039c31b874a32d7789c3be14763f628e5d
Harshit Gopal Gupta <104909447+gopal414@users.noreply.github.com>
2022-07-07 04:36:36 +0530

    Time: 7 ms (16.86%), Space: 7 MB (92.54%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    void swap(int* a, int* b)
+/*    void swap(int* a, int* b)
 {
     int temp = *a;
     *a = *b;
     *b = temp;
-}
+}*/
     void rotate(vector<vector<int>>& matrix) {
         int n=matrix.size();
         for(int i=1;i<n;i++){
             for(int j=0;j<i;j++){
-               swap( &matrix[i][j],&matrix[j][i]);
+               swap( matrix[i][j], matrix[j][i]);
             }
         }
         for(int k=0;k<n;k++){
             reverse(matrix[k].begin(),matrix[k].end());
-        }
-        
+        }       
         
     }
 };
