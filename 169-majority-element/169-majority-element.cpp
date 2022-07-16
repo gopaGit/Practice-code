b00f3c4421f48442592b6272fb4f9f85e5b8a9bd
Harshit Gopal Gupta <104909447+gopal414@users.noreply.github.com>
2022-07-16 14:13:37 +0530

    Time: 32 ms (44.18%), Space: 19.6 MB (58.28%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -3,15 +3,11 @@ public:
     int majorityElement(vector<int>& nums) {
         int n=nums.size();
         unordered_map<int,int>mp;
-        int cnt=0,ans=0;
+      //  int cnt=0,ans=0;
         for(auto x: nums){
-            mp[x]++;
-            if(mp[x]>cnt){
-                cnt=mp[x];
-                ans=x;
-            }
+            if(++mp[x]>(n/2))
+                return x;
         }
-        return ans;
-        
+       return 0; 
     }
 };
